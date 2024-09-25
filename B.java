5a27e0b8299dba701d049ef5a8477a7d8cbf3c36
narasimha1330 <narasimhaorchu6@gmail.com>
2024-09-24 23:40:48 -0700

    Add files via upload

---
+++ B.java
@@ -0,0 +1,21 @@
+public class DebugExample
+{
+public static void main(String[] args)
+{
+int n=5;
+int sum=0;
+for(int i=1;i<=n;i++)
+{
+if(i%2==0)
+{
+sum+=i;
+}
+else
+{
+sum=i;
+}
+}
+System.out.println("Sum:"+Sum);
+}
+}
+ 
